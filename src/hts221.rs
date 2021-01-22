0e6d4960f8ba611ddcc846ef4f5a5912b5166017
Bob McWhirter <bmcwhirt@redhat.com>
2021-01-22 11:52:24 -0500

    Calibrate readings from the HTS221.

--- src/hts221.rs
+++ src/hts221.rs
@@ -10,8 +10,27 @@ const WRITE: u8 = 0xBE;
 const READ: u8 = 0xBF;
 
 const WHO_AM_I: u8 = 0x0F;
-const TEMP_OUT_L: u8 = 0x2A;
-const TEMP_OUT_H: u8 = 0x2B;
+
+const T_OUT_L: u8 = 0x2A;
+const T_OUT_H: u8 = 0x2B;
+// auto-increment variant
+const T_OUT: u8 = 0xAA;
+
+const T0_OUT_L: u8 = 0x3C;
+const T0_OUT_H: u8 = 0x3D;
+// auto-increment variant
+const T0_OUT: u8 = 0xBC;
+
+const T0_DEGC_X8: u8 = 0x32;
+
+const T1_OUT_L: u8 = 0x3E;
+const T1_OUT_H: u8 = 0x3F;
+// auto-increment variant
+const T1_OUT: u8 = 0xBE;
+
+const T1_DEGC_X8: u8 = 0x33;
+
+const T1_T0_MSB: u8 = 0x35;
 
 pub struct Hts221<I: WriteRead + Read + Write>
     where <I as WriteRead>::Error: Debug,
@@ -29,6 +48,72 @@ impl<I: WriteRead + Read + Write> Hts221<I>
             i2c: None
         }
     }
+
+    fn read_t_out(i2c: &mut I) -> i16 {
+        let mut buf = [0; 2];
+        i2c.write_read(ADDR, &[T_OUT], &mut buf);
+        i16::from_le_bytes(buf)
+    }
+
+    fn read_t0_out(i2c: &mut I) -> i16 {
+        let mut buf = [0; 2];
+        i2c.write_read(ADDR, &[T0_OUT], &mut buf);
+        i16::from_le_bytes(buf)
+    }
+
+    fn read_t0_degc_x8(i2c: &mut I) -> u8 {
+        let mut buf = [0; 1];
+        i2c.write_read(ADDR, &[T0_DEGC_X8], &mut buf);
+        buf[0]
+    }
+
+    fn read_t1_out(i2c: &mut I) -> i16 {
+        let mut buf = [0; 2];
+        i2c.write_read(ADDR, &[T1_OUT], &mut buf);
+        i16::from_le_bytes(buf)
+    }
+
+    fn read_t1_degc_x8(i2c: &mut I) -> u8 {
+        let mut buf = [0; 1];
+        i2c.write_read(ADDR, &[T1_DEGC_X8], &mut buf);
+        buf[0]
+    }
+
+    fn read_t1_t0_msb(i2c: &mut I) -> (u8, u8) {
+        let mut buf = [0; 1];
+        i2c.write_read(ADDR, &[T1_T0_MSB], &mut buf);
+
+        let t0_msb = (buf[0] & 0b00000011) >> 2;
+        let t1_msb = (buf[0] & 0b00001100) >> 2;
+
+        (t1_msb, t0_msb)
+    }
+
+    fn calibrated_temperature_degc(i2c: &mut I) -> f32 {
+        let t_out = Self::read_t_out(i2c);
+
+        let t0_out = Self::read_t0_out(i2c);
+        let t1_out = Self::read_t1_out(i2c);
+
+        let t0_degc = Self::read_t0_degc_x8(i2c);
+        let t1_degc = Self::read_t1_degc_x8(i2c);
+
+        let (t1_msb, t0_msb) = Self::read_t1_t0_msb(i2c);
+
+        let t0_degc = i16::from_le_bytes([t0_degc, t0_msb]) as f32 / 8.0;
+        let t1_degc = i16::from_le_bytes([t1_degc, t1_msb]) as f32 / 8.0;
+
+        let slope = (t1_degc - t0_degc ) / (t1_out - t0_out ) as f32;
+
+        let t_degc = t0_degc as f32 + (slope * (t_out - t0_out) as f32);
+
+        t_degc
+    }
+
+    fn c_to_f(c: f32) -> f32 {
+        c * (9.0/5.0) + 32.0
+    }
+
 }
 
 impl<I: WriteRead + Read + Write> Actor for Hts221<I>
@@ -43,7 +128,6 @@ impl<I: WriteRead + Read + Write> NotificationHandler<SetI2c<I>> for Hts221<I>
           <I as Write>::Error: Debug
 {
     fn on_notification(&'static mut self, message: SetI2c<I>) -> Completion {
-        log::info!("wired up i2c");
         self.i2c.replace(message.0);
         Completion::immediate()
     }
@@ -57,49 +141,10 @@ impl<I: WriteRead + Read + Write> NotificationHandler<TakeReading> for Hts221<I>
 {
     fn on_notification(&'static mut self, message: TakeReading) -> Completion {
         Completion::defer(async move {
-            log::info!("taking reading --- dude");
-            let mut i2c = self.i2c.as_mut().unwrap().lock().await;
-
-
-            let buf = [CTRL_REG1, 1 << 7];
-            let result = i2c.write(ADDR, &buf);
-            match result {
-                Ok(_) => {
-                    log::info!("### GOOD write CTRL_REG1");
-                }
-                Err(e) => {
-                    log::info!("### BAD write CTRL_REG1 {:?}", e);
-                }
+            if let Some(ref i2c) = self.i2c {
+                let temp_degc = Self::calibrated_temperature_degc(&mut i2c.lock().await as &mut I);
+                log::info!(" ** The temperature is {} °F", Self::c_to_f( temp_degc) );
             }
-
-            let buf = [TEMP_OUT_L];
-            let mut temp = [0; 1];
-            let result = i2c.write_read(ADDR, &buf, &mut temp);
-            match result {
-                Ok(_) => {
-                    log::info!("### GOOD write_read");
-                }
-                Err(e) => {
-                    log::info!("### BAD write_read {:?}", e);
-                }
-            }
-
-            log::info!("***************** temp L {}", temp[0]);
-
-            let buf = [TEMP_OUT_H];
-            let mut temp = [0; 1];
-            let result = i2c.write_read(ADDR, &buf, &mut temp);
-            match result {
-                Ok(_) => {
-                    log::info!("### GOOD write_read");
-                }
-                Err(e) => {
-                    log::info!("### BAD write_read {:?}", e);
-                }
-            }
-
-            log::info!("***************** temp H {}", temp[0]);
-
         })
     }
 }
